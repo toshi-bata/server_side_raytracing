ac7e38822656bacaae6d971b17394cfa6d30f976
Toshiaki Kawabata <toshiaki@techsoft3d.com>
2025-02-05 12:23:50 +0900

    Ported 2025.1.0

--- ExLuServer/_VS2019.bat
+++ ExLuServer/_VS2019.bat
@@ -1,6 +1,6 @@
 SET LIBMICRO_INSTALL_DIR=C:\SDK\libmicrohttpd\libmicrohttpd-1.0.0-w32-bin
-SET HEXCHANGE_INSTALL_DIR=C:\SDK\Exchange_Publish\HOOPS_Exchange_Publish_2024.7.0
-SET HCOMMUNICATOR_INSTALL_DIR=C:\SDK\Communicator\HOOPS_Communicator_2024.7.1
-SET HLUMINATE_INSTALL_DIR=C:\SDK\Luminate\HOOPS_Luminate_2024.7.0
+SET HEXCHANGE_INSTALL_DIR=C:\SDK\Exchange_Publish\HOOPS_Exchange_Publish_2025.1.0
+SET HCOMMUNICATOR_INSTALL_DIR=C:\SDK\Communicator\HOOPS_Communicator_2025.1.0
+SET HLUMINATE_INSTALL_DIR=C:\SDK\Luminate\HOOPS_Luminate_2025.1.0
 
 CALL "C:\Program Files (x86)\Microsoft Visual Studio\2019\Professional\Common7\IDE\devenv.exe" ExLuServer.sln
